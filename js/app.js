e71aff0f99c900a7a811c8a0e9d893687c27c7a2
Junaid Abbasi <my.github.juni.abbasi@icloud.com>
2021-07-22 01:23:42 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,56 @@
+// global variables
+const mainNav = document.getElementById('js-menu');
+const navBarToggle = document.getElementById('js-navbar-toggle');
+const cardSections = document.querySelectorAll('section');
+const documentFrag = document.createDocumentFragment();
+const navLi = document.querySelector('.navbar-menu ul li');
+
+// making hamburger visible when clicking
+navBarToggle.addEventListener('click', function() {
+    mainNav.classList.toggle('active-ham');
+});
+
+// build an unordered list 
+cardSections.forEach(section => {
+    const li = document.createElement('li');
+    const anchor = document.createElement('a');
+    anchor.innerHTML = section.getAttribute('data-nav');
+    anchor.classList.add('nav-links');
+    // anchor.classList.add('active');
+    anchor.classList.add(section.id);
+    anchor.href = `#${section.id}`;
+    li.appendChild(anchor);
+    documentFrag.appendChild(li);
+});
+
+mainNav.appendChild(documentFrag);
+
+// stick nav when scrolling
+const mainHeader = document.querySelector('.navbar-menu');
+let sticky = mainHeader.offsetTop;
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (window.pageYOffset >= sticky) {
+        mainHeader.classList.add("sticky");
+    }
+    else {
+        mainHeader.classList.remove("sticky");
+    }
+}
+
+// active nav when in section 
+function activeSection(){
+  for(let section of cardSections){ 
+    const item = section.getBoundingClientRect();
+    const activeElement = document.getElementById(section.id);
+    if (item.y <= 90 && item.bottom >= 150){
+      section.classList.add("active");
+    }
+    else {
+      section.classList.remove("active");
+    }
+  }
+}
+document.addEventListener('scroll', activeSection);
